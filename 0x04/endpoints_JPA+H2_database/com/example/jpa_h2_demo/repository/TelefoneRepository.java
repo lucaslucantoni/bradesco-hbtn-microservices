e96228349fabf0f2afb3204c835956076ce505c4
Lucas Lucantoni <lucas.henrique.s.go@gmail.com>
2025-10-12 12:58:23 -0300

    Files

--- 0x04/endpoints_JPA+H2_database/com/example/jpa_h2_demo/repository/TelefoneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpa_h2_demo.repository;
-
-import com.example.jpa_h2_demo.model.Telefone;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
-}
